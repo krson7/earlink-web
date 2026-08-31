2ec8dba40ca2ffcba54e46fad0d5e0d04c039493
krson7 <gallarysonny7@gmail.com>
2026-08-31 14:35:48 +0900

    로딩바 위치변경

--- src/components/entry/IntroSplash.tsx
+++ src/components/entry/IntroSplash.tsx
@@ -45,7 +45,7 @@ export default function IntroSplash() {
           />
 
           {/* 진행 표시 */}
-         <div className="mt-2 w-full max-w-[270px]">
+         <div className="mt-1 w-full max-w-[240px]">
             {/* 로딩 바 */}
             <div
               role="progressbar"
